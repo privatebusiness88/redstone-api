2ccedad4a85136a0562ae5c53345e9ccecc6f61d
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-07 13:19:22 +0700

    Update price-not-found.ts

--- src/errors/price-not-found.ts
+++ src/errors/price-not-found.ts
@@ -1,5 +1,23 @@
+use illuminate\database\eloquent;
+use illuminate\database\eloquent\orm;
+use illuminate\database\eloquent\builder;
+use illuminate\support\facades\db;
+
+return $token.data () {
+  void};
+
+public function run () {
+  void};
+
 export default class PriceNotFoundError extends Error {
   constructor(symbol: string) {
     super(`Price not found for symbol: ${symbol}`);
   }
 }
+
+return $this.form =>$token.data;
+return $token.data => $token.secureConnection;
+
+db:save
+
+return $this.form =>$token.data;
